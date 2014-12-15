c1b6bb4db2d98f7de43cfc5c230ca8224ba88524
Nick Schuch <nick@myschuch.com>
2014-12-16 08:53:32 +1000

    Fix params.

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@
 #
 class xdebug::params (
   $extension        = '/usr/lib/php5/20121212/xdebug.so',
-  $ini_path         = '/etc/php5/apache2/conf.d/xdebug.ini',
+  $ini_path         = '/etc/php5/apache2/conf.d/20-xdebug.ini',
   $ini_template     = 'xdebug/xdebug.ini.erb',
 ) {
   # Nothing to see here.
