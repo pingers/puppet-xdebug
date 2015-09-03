267f6f7835536ceea61f06577fd6c1e072a93f58
Michael Priest <pingers@gmail.com>
2015-09-03 13:50:04 +0930

    Use php56 path to xdebug.so.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,7 @@
 # Parameters for xdebug.
 #
 class xdebug::params (
-  $extension = '/usr/lib/php5/20121212/xdebug.so',
+  $extension = '/usr/lib/php5/20131226/xdebug.so',
   $ini       = 'xdebug/xdebug.ini.erb',
 ) {
   # Nothing to see here.
